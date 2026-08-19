a6f8e6cca8ecd1a7033cc85891d360d2ef55b904
Elangovan <elango.jmc@gmail.com>
2026-08-19 17:24:07 +0530

    Initial Commit

---
+++ MoneyLens/App/MoneyLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyLensApp.swift
+//  MoneyLens
+//
+//  Created by Pooja Elangovan on 19/08/26.
+//
+
+import SwiftUI
+
+@main
+struct MoneyLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MoneyLensTests/MoneyLensTests.swift
@@ -0,0 +1,19 @@
+//
+//  MoneyLensTests.swift
+//  MoneyLensTests
+//
+//  Created by Pooja Elangovan on 19/08/26.
+//
+
+import Testing
+@testable import MoneyLens
+
+struct MoneyLensTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
